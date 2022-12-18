0fbac468899d68d37978da8599085a121f78db53
marcelymelo <marcelyjfmelo@gmail.com>
2022-12-18 17:17:13 -0300

    Delete

--- jdbc/crud/src/br/com/agenda/app/Main.java
+++ jdbc/crud/src/br/com/agenda/app/Main.java
@@ -10,18 +10,21 @@ public class Main {
 	public static void main(String[] args) {
 		Contato contato = new Contato();
 		ContatoDAO contatoDao = new ContatoDAO();
-//		contato.setNome("Adriel");
-//		contato.setIdade(19);
+//		contato.setNome("Pombo");
+//		contato.setIdade(24);
 //		contato.setDataCadastro(new Date());
 //		contatoDao.save(contato);
 		
 		//Atualizar Contato
-		Contato c1 = new Contato();
-		c1.setNome("Maria Eduarda");
-		c1.setIdade(22);
-		c1.setDataCadastro(new Date());
-		c1.setId(2);
-		contatoDao.update(c1);
+//		Contato c1 = new Contato();
+//		c1.setNome("Maria Eduarda");
+//		c1.setIdade(22);
+//		c1.setDataCadastro(new Date());
+//		c1.setId(2);
+//		contatoDao.update(c1);
+		
+		contatoDao.deleteByID(7);
+		
 		
 		for(Contato c : contatoDao.getContatos()) {
 			System.out.println("Contato");
